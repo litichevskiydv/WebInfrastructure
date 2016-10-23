8bc4ea7967e73c24d4c0a4181db6f5474a25dde4
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-10-23 14:58:49 +0500

    Packages creation options were added

---
+++ build/CreatePackages.bat
@@ -0,0 +1,4 @@
+dotnet pack "src\Infrastructure\Skeleton.Domain" -c %1 --version-suffix %2
+dotnet pack "src\Infrastructure\Skeleton.Integrations" -c %1 --version-suffix %2
+dotnet pack "src\Infrastructure\Skeleton.Web" -c %1 --version-suffix %2
+dotnet pack "src\Infrastructure\Skeleton.Web.Testing" -c %1 --version-suffix %2
