8157c4f4142683a187a4730d91b49fb14b1d53f5
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2016-11-03 00:25:09 +0500

    SqlMetaData creators for TVP columns were declared

--- src/Infrastructure/Skeleton.Dapper/Tvp/SqlTypesMapper.cs
+++ src/Infrastructure/Skeleton.Dapper/Tvp/SqlTypesMapper.cs
@@ -3,10 +3,15 @@
     using System;
     using System.Collections.Generic;
     using System.Data;
+    using Microsoft.SqlServer.Server;
 
     public static class SqlTypesMapper
     {
         private static readonly Dictionary<Type, SqlDbType> SqlTypesMappings;
+        private static readonly Dictionary<Type, Func<string, MetaDataCreationOptions, SqlMetaData>> SqlMetaDataCreators;
+
+        private static readonly MetaDataCreationOptions DefaultArraysMetaDataCreationOptions;
+        private static readonly MetaDataCreationOptions DefaultDecimalMetaDataCreationOptions;
 
         private static Dictionary<Type, SqlDbType> AddMap<TValue>(this Dictionary<Type, SqlDbType> mappings, SqlDbType sqlDbType)
         {
@@ -18,6 +23,56 @@
                 var nullableType = typeof(Nullable<>).MakeGenericType(type);
                 mappings[nullableType] = sqlDbType;
             }
+
+            return mappings;
+        }
+        private static SqlMetaData CreatorForScalarTypes(string columnName, SqlDbType sqlDbType, MetaDataCreationOptions metaDataCreationOptions)
+        {
+            return new SqlMetaData(columnName, sqlDbType);
+        }
+
+        private static SqlMetaData CreatorForChar(string columnName, SqlDbType sqlDbType, MetaDataCreationOptions metaDataCreationOptions)
+        {
+            return new SqlMetaData(columnName, sqlDbType, 1);
+        }
+
+        private static SqlMetaData CreatorForDecimal(string columnName, SqlDbType sqlDbType, MetaDataCreationOptions metaDataCreationOptions)
+        {
+            if (metaDataCreationOptions != null && metaDataCreationOptions.Precision.HasValue == false)
+                throw new ArgumentNullException(nameof(metaDataCreationOptions.Precision));
+            if (metaDataCreationOptions != null && metaDataCreationOptions.Scale.HasValue == false)
+                throw new ArgumentNullException(nameof(metaDataCreationOptions.Scale));
+            var options = metaDataCreationOptions ?? DefaultDecimalMetaDataCreationOptions;
+
+            return new SqlMetaData(columnName, sqlDbType, options.Precision.Value, options.Scale.Value);
+        }
+
+        private static SqlMetaData CreatorForArrays(string columnName, SqlDbType sqlDbType, MetaDataCreationOptions metaDataCreationOptions)
+        {
+            if (metaDataCreationOptions != null && metaDataCreationOptions.MaxLength.HasValue == false)
+                throw new ArgumentNullException(nameof(metaDataCreationOptions.MaxLength));
+
+            return new SqlMetaData(columnName, sqlDbType, (metaDataCreationOptions ?? DefaultArraysMetaDataCreationOptions).MaxLength.Value);
+        }
+
+        private static SqlMetaData CreatorForObject(string columnName, SqlDbType sqlDbType, MetaDataCreationOptions metaDataCreationOptions)
+        {
+            return new SqlMetaData(columnName, sqlDbType);
+        }
+
+        private static Dictionary<Type, Func<string, MetaDataCreationOptions, SqlMetaData>> AddCreator<TValue>(
+            this Dictionary<Type, Func<string, MetaDataCreationOptions, SqlMetaData>> mappings,
+            Func<string, SqlDbType, MetaDataCreationOptions, SqlMetaData> creator)
+        {
+            var type = typeof(TValue);
+            mappings[type] = (columnName, options) => creator(columnName, GetSqlType(type), options);
+
+            if (default(TValue) != null)
+            {
+                var nullableType = typeof(Nullable<>).MakeGenericType(type);
+                mappings[nullableType] = (columnName, options) => creator(columnName, GetSqlType(nullableType), options);
+            }
+
             return mappings;
         }
 
@@ -41,11 +96,43 @@
                 .AddMap<char[]>(SqlDbType.NVarChar)
                 .AddMap<string>(SqlDbType.NVarChar)
                 .AddMap<object>(SqlDbType.Variant);
+
+            DefaultArraysMetaDataCreationOptions = new MetaDataCreationOptions {MaxLength = -1};
+            DefaultDecimalMetaDataCreationOptions = new MetaDataCreationOptions {Precision = 10, Scale = 2};
+            SqlMetaDataCreators = new Dictionary<Type, Func<string, MetaDataCreationOptions, SqlMetaData>>()
+                .AddCreator<byte>(CreatorForScalarTypes)
+                .AddCreator<short>(CreatorForScalarTypes)
+                .AddCreator<char>(CreatorForChar)
+                .AddCreator<int>(CreatorForScalarTypes)
+                .AddCreator<long>(CreatorForScalarTypes)
+                .AddCreator<float>(CreatorForScalarTypes)
+                .AddCreator<double>(CreatorForScalarTypes)
+                .AddCreator<decimal>(CreatorForDecimal)
+                .AddCreator<bool>(CreatorForScalarTypes)
+                .AddCreator<Guid>(CreatorForScalarTypes)
+                .AddCreator<DateTime>(CreatorForScalarTypes)
+                .AddCreator<DateTimeOffset>(CreatorForScalarTypes)
+                .AddCreator<byte[]>(CreatorForArrays)
+                .AddCreator<string>(CreatorForArrays)
+                .AddCreator<object>(CreatorForObject);
         }
 
         public static SqlDbType GetSqlType(Type type)
         {
-            return SqlTypesMappings[type];
+            SqlDbType dbType;
+            if (SqlTypesMappings.TryGetValue(type, out dbType) == false)
+                throw new InvalidOperationException($"No SqlDbType mapping was found for {type}");
+
+            return dbType;
+        }
+
+        public static SqlMetaData CreateSqlMetaData(string columnName, Type type, MetaDataCreationOptions metaDataCreationOptions)
+        {
+            Func<string, MetaDataCreationOptions, SqlMetaData> creator;
+            if (SqlMetaDataCreators.TryGetValue(type, out creator) == false)
+                throw new InvalidOperationException($"No SqlMetaData was found for {type}");
+
+            return creator(columnName, metaDataCreationOptions);
         }
     }
 }
