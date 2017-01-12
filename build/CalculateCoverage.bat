c8b6fa7c9b46489d3100ba97dc49654e9e1a9e01
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-01-12 20:35:07 +0500

    Checks for test runner results were added

--- build/CalculateCoverage.bat
+++ build/CalculateCoverage.bat
@@ -1,11 +1,16 @@
 powershell.exe -file build\SetFullDebugType.ps1
-
 nuget install OpenCover -ExcludeVersion -OutputDirectory tools
+
 tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\Infrastructure\Skeleton.Common.Tests"" -c Debug" -register:user -filter:"+[Skeleton*]* -[xunit*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:Filter;Attribute -threshold:100 -oldstyle -returntargetcode -output:coverage.xml
+if %errorlevel% neq 0 exit /b %errorlevel%
 tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\Infrastructure\Skeleton.CQRS.Implementations.Tests"" -c Debug" -register:user -filter:"+[Skeleton*]* -[xunit*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:Filter;Attribute -threshold:100 -oldstyle -returntargetcode -mergeoutput -output:coverage.xml
+if %errorlevel% neq 0 exit /b %errorlevel%
 tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\Infrastructure\Skeleton.Dapper.Tests"" -c Debug" -register:user -filter:"+[Skeleton*]* -[xunit*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:Filter;Attribute -threshold:100 -oldstyle -returntargetcode -mergeoutput -output:coverage.xml
+if %errorlevel% neq 0 exit /b %errorlevel%
 tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\Infrastructure\Skeleton.Web.Tests"" -c Debug" -register:user -filter:"+[Skeleton*]* -[xunit*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:Filter;Attribute -threshold:100 -oldstyle -returntargetcode -mergeoutput -output:coverage.xml
+if %errorlevel% neq 0 exit /b %errorlevel%
 tools\OpenCover\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:" test ""test\Web.Tests"" -c Debug" -register:user -filter:"+[Skeleton*]* -[xunit*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:Filter;Attribute -threshold:100 -oldstyle -returntargetcode -mergeoutput -output:coverage.xml
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
 pip install codecov
--- build/RunTests.bat
+++ build/RunTests.bat
@@ -1,5 +1,10 @@
 dotnet test "test\Infrastructure\Skeleton.Common.Tests" -c %1
+if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet test "test\Infrastructure\Skeleton.CQRS.Implementations.Tests" -c %1
+if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet test "test\Infrastructure\Skeleton.Dapper.Tests" -c %1
+if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet test "test\Infrastructure\Skeleton.Web.Tests" -c %1
+if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet test "test\Web.Tests" -c %1
+if %errorlevel% neq 0 exit /b %errorlevel%
