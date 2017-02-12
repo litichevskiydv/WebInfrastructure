c388b88f9657a0b064db9e5214e703619b04279c
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-02-12 17:49:53 +0500

    Pipeline builder extension with explicit options passing was added

--- src/Infrastructure/Skeleton.Web.Authentication/JwtBearer/ApplicationBuilderExtensions.cs
+++ src/Infrastructure/Skeleton.Web.Authentication/JwtBearer/ApplicationBuilderExtensions.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Options;
 
     public static class ApplicationBuilderExtensions
     {
@@ -13,6 +14,21 @@
             return applicationBuilder
                 .UseMiddleware<TokensIssuingMiddleware>()
                 .UseJwtBearerAuthentication();
+        }
+
+        public static IApplicationBuilder UseJwtBearerAuthorisationTokens(this IApplicationBuilder applicationBuilder,
+            TokensIssuingOptions issuingOptions, JwtBearerOptions verificationOptions)
+        {
+            if (applicationBuilder == null)
+                throw new ArgumentNullException(nameof(applicationBuilder));
+            if (issuingOptions == null)
+                throw new ArgumentNullException(nameof(issuingOptions));
+            if(verificationOptions == null)
+                throw new ArgumentNullException(nameof(verificationOptions));
+
+            return applicationBuilder
+                .UseMiddleware<TokensIssuingMiddleware>(Options.Create(issuingOptions))
+                .UseJwtBearerAuthentication(verificationOptions);
         }
     }
 }
