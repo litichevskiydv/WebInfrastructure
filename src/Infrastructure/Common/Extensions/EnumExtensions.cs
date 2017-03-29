846be545e3b52f6102dca755d128e725ac0c9617
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-03-29 08:45:20 +0500

    Extension for getting DescriptionAttribute value from enum member was added

---
+++ src/Infrastructure/Common/Extensions/EnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Skeleton.Common.Extensions
+{
+    using System;
+    using System.ComponentModel;
+    using System.Reflection;
+
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum member)
+        {
+            var memberTypeInfo = member.GetType().GetTypeInfo();
+
+            if (memberTypeInfo.IsEnum == false)
+                throw new ArgumentOutOfRangeException(nameof(member), "member is not enum");
+
+            var fieldInfo = memberTypeInfo.GetField(member.ToString());
+            if (fieldInfo == null)
+                return null;
+
+            var attributes = fieldInfo.GetCustomAttributes<DescriptionAttribute>(false).AsArray();
+
+            if (attributes.Length > 0)
+                return attributes[0].Description;
+
+            return member.ToString();
+        }
+    }
+}
---
+++ test/Infrastructure/Common.Tests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Skeleton.Common.Tests.Extensions
+{
+    using System.ComponentModel;
+    using Common.Extensions;
+    using Xunit;
+
+    public class EnumExtensionsTests
+    {
+        private enum Marks
+        {
+            [Description("Bad result")]
+            One
+        }
+
+        [Fact]
+        public void ShouldGetDescriptionForEnumMember()
+        {
+            //Given
+            const string expectedDescription = "Bad result";
+
+            // When
+            var actualDescription = Marks.One.GetDescription();
+
+            // Then
+            Assert.Equal(expectedDescription, actualDescription);
+        }
+    }
+}
