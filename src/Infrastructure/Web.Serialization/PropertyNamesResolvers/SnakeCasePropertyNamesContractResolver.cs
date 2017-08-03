abfa13913a704b3b3c5ca7a825a470b512f566dd
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-08-03 23:31:26 +0500

    Property names contract resolver was added for snake case was added

---
+++ src/Infrastructure/Web.Serialization/PropertyNamesResolvers/SnakeCasePropertyNamesContractResolver.cs
@@ -0,0 +1,12 @@
+﻿namespace Skeleton.Web.Serialization.PropertyNamesResolvers
+{
+    using Newtonsoft.Json.Serialization;
+
+    public class SnakeCasePropertyNamesContractResolver : DefaultContractResolver
+    {
+        public SnakeCasePropertyNamesContractResolver() : base(true)
+        {
+            NamingStrategy = new SnakeCaseNamingStrategy(true, true);
+        }
+    }
+}
---
+++ test/Infrastructure/Web.Tests/SnakeCasePropertyNamesContractResolverTests.cs
@@ -0,0 +1,53 @@
+﻿namespace Skeleton.Web.Tests
+{
+    using JetBrains.Annotations;
+    using Newtonsoft.Json;
+    using Serialization;
+    using Serialization.PropertyNamesResolvers;
+    using Xunit;
+
+    public class SnakeCasePropertyNamesContractResolverTests
+    {
+        [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+        private class Request
+        {
+            public string RepoName { get; set; }
+
+            public string Data { get; set; }
+
+            protected bool Equals(Request other)
+            {
+                return string.Equals(RepoName, other.RepoName) && string.Equals(Data, other.Data);
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((Request) obj);
+            }
+        }
+
+        private readonly JsonSerializerSettings _serializerSettings;
+
+        public SnakeCasePropertyNamesContractResolverTests()
+        {
+            _serializerSettings = new JsonSerializerSettings().UseContractResolver(new SnakeCasePropertyNamesContractResolver());
+        }
+
+        [Fact]
+        public void ShouldDeserializeData()
+        {
+            // Given
+            const string requestString = @"{ ""repo_name"":""SomeName"", ""data"":""SomeData"" }";
+            var expectedRequest = new Request {RepoName = "SomeName", Data = "SomeData"};
+
+            // When
+            var actualRequest = JsonConvert.DeserializeObject<Request>(requestString, _serializerSettings);
+
+            // Then
+            Assert.Equal(expectedRequest, actualRequest);
+        }
+    }
+}
