8fadc215cd58d519216f80803f316e9931a1c2e3
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-09-23 23:22:40 +0500

    Jil Serialize method was called for proper types

--- src/Infrastructure/Web.Serialization/Jil/Formatters/JilOutputFormatter.cs
+++ src/Infrastructure/Web.Serialization/Jil/Formatters/JilOutputFormatter.cs
@@ -1,6 +1,10 @@
 ﻿namespace Skeleton.Web.Serialization.Jil.Formatters
 {
     using System;
+    using System.IO;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using System.Runtime.ExceptionServices;
     using System.Text;
     using System.Threading.Tasks;
     using global::Jil;
@@ -9,6 +13,7 @@
     public class JilOutputFormatter : TextOutputFormatter
     {
         private readonly Options _options;
+        private readonly MethodInfo _serializeGenericDefinition;
 
         public JilOutputFormatter(Options options)
         {
@@ -16,6 +21,10 @@
                 throw new ArgumentNullException(nameof(options));
 
             _options = options;
+
+            Expression<Action<object, TextWriter, Options>> fakeSerializeCall =
+                (data, writer, settings) => JSON.Serialize(data, writer, settings);
+            _serializeGenericDefinition = ((MethodCallExpression)fakeSerializeCall.Body).Method.GetGenericMethodDefinition();
 
             SupportedEncodings.Add(Encoding.UTF8);
             SupportedEncodings.Add(Encoding.Unicode);
@@ -34,7 +43,16 @@
             var response = context.HttpContext.Response;
             using (var writer = context.WriterFactory(response.Body, selectedEncoding))
             {
-                JSON.Serialize(context.Object, writer, _options);
+                try
+                {
+                    _serializeGenericDefinition
+                        .MakeGenericMethod(context.ObjectType)
+                        .Invoke(null, new[] {context.Object, writer, _options});
+                }
+                catch (TargetInvocationException ex)
+                {
+                    ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
+                }
                 await writer.FlushAsync();
             }
         }
