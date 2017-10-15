710c9917086eb090e17a080696a889712efa8987
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-10-15 17:08:04 +0500

    NLog logger was replaced by Serilog

---
+++ src/Infrastructure/Web/Logging/Serilog/WebHostBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Skeleton.Web.Logging.Serilog
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using global::Serilog;
+    using Microsoft.AspNetCore.Hosting;
+
+    public static class WebHostBuilderExtensions
+    {
+        [ExcludeFromCodeCoverage]
+        public static IWebHostBuilder UseSerilog(
+            this IWebHostBuilder hostBuilder,
+            Action<LoggerConfiguration> configureLogging
+        )
+        {
+            if(hostBuilder == null)
+                throw new ArgumentNullException(nameof(hostBuilder));
+            if (configureLogging == null)
+                throw new ArgumentNullException(nameof(configureLogging));
+
+            var loggerConfiguration = new LoggerConfiguration();
+            hostBuilder.ConfigureServices((context, collection) =>
+                                          {
+                                              configureLogging(loggerConfiguration);
+                                              loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+
+                                              Log.Logger = loggerConfiguration.CreateLogger();
+                                          });
+            return hostBuilder.UseSerilog();
+        }
+    }
+}
