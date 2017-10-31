e6770aea5b8b6043c80f993b0d14631e636b8f72
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-11-01 00:19:50 +0500

    Input formatter based on protobuf-net was implemented

---
+++ src/Infrastructure/Web.Serialization/ProtobufNet/Formatters/ProtobufInputFormatter.cs
@@ -0,0 +1,54 @@
+﻿namespace Skeleton.Web.Serialization.ProtobufNet.Formatters
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc.Formatters;
+    using Microsoft.Extensions.Logging;
+    using ProtoBuf.Meta;
+
+    public class ProtobufInputFormatter : InputFormatter
+    {
+        private readonly RuntimeTypeModel _runtimeTypeModel;
+        private readonly ILogger<ProtobufInputFormatter> _logger;
+
+        private static RuntimeTypeModel CreateRuntimeTypeModel()
+        {
+            var runtimeTypeModel = TypeModel.Create();
+            runtimeTypeModel.UseImplicitZeroDefaults = false;
+
+            return runtimeTypeModel;
+        }
+
+        public ProtobufInputFormatter(ILogger<ProtobufInputFormatter> logger)
+        {
+            if (logger == null)
+                throw new ArgumentNullException(nameof(logger));
+
+            _runtimeTypeModel = CreateRuntimeTypeModel();
+            _logger = logger;
+
+            SupportedMediaTypes.Add(MediaTypeHeaderValues.ApplicationProtobuf);
+            SupportedMediaTypes.Add(MediaTypeHeaderValues.ApplicationProtobufSynonym);
+        }
+
+        public override Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            object model;
+
+            try
+            {
+                model = _runtimeTypeModel.Deserialize(context.HttpContext.Request.Body, null, context.ModelType);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogDebug(exception, "Exception was occurred during deserialization");
+                return InputFormatterResult.FailureAsync();
+            }
+
+            return InputFormatterResult.SuccessAsync(model);
+        }
+    }
+}
