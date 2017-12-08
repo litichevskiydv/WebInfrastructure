8f0b0f04cda30926a7f0894b40403a94df15a6d8
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2017-12-08 12:15:20 +0500

    Type handler for Dapper was added for writing collections as JSON

---
+++ src/Infrastructure/Dapper/TypesHandlers/CollectionsHandler.cs
@@ -0,0 +1,55 @@
+﻿namespace Skeleton.Dapper.TypesHandlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using global::Dapper;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public class CollectionsHandler<TSource> : SqlMapper.ITypeHandler
+    {
+        private readonly bool _handleEmptyCollectionAsNull;
+        private readonly JsonSerializerSettings _serializerSettings;
+        private readonly Type _baseCollectionsType;
+
+        public CollectionsHandler(bool handleEmptyCollectionAsNull, JsonSerializerSettings serializerSettings = null)
+        {
+            _handleEmptyCollectionAsNull = handleEmptyCollectionAsNull;
+            _serializerSettings = serializerSettings
+                                  ?? new JsonSerializerSettings
+                                     {
+                                         Converters = {new StringEnumConverter()},
+                                         NullValueHandling = NullValueHandling.Ignore,
+                                         DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate,
+                                         TypeNameHandling = TypeNameHandling.Auto
+                                     };
+            _baseCollectionsType = typeof(IReadOnlyCollection<TSource>);
+        }
+
+        public void SetValue(IDbDataParameter parameter, object value)
+        {
+            if (value == DBNull.Value)
+            {
+                parameter.Value = DBNull.Value;
+                return;
+            }
+
+            if (!(value is IReadOnlyCollection<TSource> collection))
+                throw new ArgumentException($"Parameter type doesn't implement {nameof(IReadOnlyCollection<TSource>)}");
+            if (collection.Count == 0 && _handleEmptyCollectionAsNull)
+                parameter.Value = DBNull.Value;
+            else
+                parameter.Value = JsonConvert.SerializeObject(value, _serializerSettings);
+        }
+
+        public object Parse(Type destinationType, object value)
+        {
+            if(_baseCollectionsType.IsAssignableFrom(destinationType) == false)
+                throw new ArgumentException($"Parameter type doesn't implement {nameof(IReadOnlyCollection<TSource>)}");
+
+            var collection = (IReadOnlyCollection<TSource>) JsonConvert.DeserializeObject(value as string, destinationType, _serializerSettings);
+            return collection.Count == 0 && _handleEmptyCollectionAsNull ? null : collection;
+        }
+    }
+}
---
+++ test/Infrastructure/Dapper.Tests/TypesHandlers/CollectionsHandlerTests.cs
@@ -0,0 +1,144 @@
+﻿namespace Skeleton.Dapper.Tests.TypesHandlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Common.Extensions;
+    using Dapper.TypesHandlers;
+    using global::Dapper;
+    using JetBrains.Annotations;
+    using Xunit;
+
+    public class CollectionsHandlerTests : DbUsingTestBase
+    {
+        public class TestEntity
+        {
+            public int Id { get; set; }
+
+            public int[] Values { get; set; }
+
+            protected bool Equals(TestEntity other)
+            {
+                return Values.IsSame(other.Values);
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((TestEntity) obj);
+            }
+        }
+
+        public class WrongEntity
+        {
+            public int Value { get; set; }
+        }
+
+        [UsedImplicitly]
+        public static IEnumerable<object[]> CollectionsHandlerUsageTestsData;
+
+        static CollectionsHandlerTests()
+        {
+            CollectionsHandlerUsageTestsData =
+                new[]
+                {
+                    new object[]
+                    {
+                        false,
+                        new TestEntity {Values = new[] {1, 2, 3}},
+                        new TestEntity {Values = new[] {1, 2, 3}}
+                    },
+                    new object[]
+                    {
+                        false,
+                        new TestEntity {Values = new int[0]},
+                        new TestEntity {Values = new int[0]}
+                    },
+                    new object[]
+                    {
+                        false,
+                        new TestEntity {Values = null},
+                        new TestEntity {Values = null}
+                    },
+                    new object[]
+                    {
+                        true,
+                        new TestEntity {Values = new[] {1, 2, 3}},
+                        new TestEntity {Values = new[] {1, 2, 3}}
+                    },
+                    new object[]
+                    {
+                        true,
+                        new TestEntity {Values = new int[0]},
+                        new TestEntity {Values = null}
+                    },
+                    new object[]
+                    {
+                        true,
+                        new TestEntity {Values = null},
+                        new TestEntity {Values = null}
+                    }
+                };
+        }
+
+        [Theory]
+        [MemberData(nameof(CollectionsHandlerUsageTestsData))]
+        public void ShouldCollectionsHandler(
+            bool handleEmptyCollectionAsNull,
+            TestEntity source,
+            TestEntity expected)
+        {
+            // Given
+            SqlMapper.AddTypeHandler(typeof(int[]), new CollectionsHandler<int>(handleEmptyCollectionAsNull));
+
+            // When
+            TestEntity actual;
+            using (var connection = SqlConnectionsFactoryMethod())
+            {
+                connection.Execute(@"create table #TestEntities (Id int identity(1, 1) not null, [Values] nvarchar(max) null)");
+                connection.Execute("insert into #TestEntities ([Values]) values(@Values)", source);
+                actual = connection.Query<TestEntity>("select * from #TestEntities").Single();
+            }
+
+            // Then
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfNotACollectionIsWritten()
+        {
+            // Given
+            SqlMapper.AddTypeHandler(typeof(WrongEntity), new CollectionsHandler<int>(false));
+
+            // When, Then
+            using (var connection = SqlConnectionsFactoryMethod())
+            {
+                connection.Execute(@"create table #TestEntities (Id int identity(1, 1) not null, [Value] nvarchar(max) null)");
+                Assert.Throws<ArgumentException>(
+                    () => connection.Execute("insert into #TestEntities ([Value]) values(@Value)",
+                        new {Value = new WrongEntity {Value = 1}})
+                );
+            }
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfNotACollectionIsRead()
+        {
+            // Given
+            SqlMapper.AddTypeHandler(typeof(WrongEntity), new CollectionsHandler<int>(false));
+
+            // When, Then
+            using (var connection = SqlConnectionsFactoryMethod())
+            {
+                connection.Execute(@"
+create table #TestEntities (Id int identity(1, 1) not null, [Value] nvarchar(max) null)
+insert into #TestEntities ([Value]) values('[1, 2,3]')");
+                Assert.Throws<ArgumentException>(
+                    () => connection.Query<WrongEntity>("select * from #TestEntities").ToArray()
+                );
+            }
+        }
+    }
+}
