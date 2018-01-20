9a6950855ef6c36ecd70fb9abed785bae51798a8
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-01-21 01:56:10 +0500

    Actions filter for models state validation was added

---
+++ src/Infrastructure/Web/Validation/ModelValidationFilterAttribute.cs
@@ -0,0 +1,29 @@
+﻿namespace Skeleton.Web.Validation
+{
+    using System.Linq;
+    using Conventions.Responses;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public class ModelValidationFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if(context.ModelState.IsValid)
+                return;
+
+            var modelErrors = context.ModelState.Values
+                .SelectMany(x => x.Errors)
+                .Select(x =>
+                            new ApiResponseError
+                            {
+                                Detail = string.IsNullOrWhiteSpace(x.Exception?.Message) == false
+                                    ? x.Exception.Message
+                                    : x.ErrorMessage
+                            }
+                )
+                .ToArray();
+            context.Result = new BadRequestObjectResult(ApiResponse.Error(modelErrors));
+        }
+    }
+}
