83461cddbcce9064908f87a3f120e0a96f782c0e
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-03-04 14:45:38 +0500

    Extensions for dictionaries were added

---
+++ src/Infrastructure/Common/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Skeleton.Common.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class DictionaryExtensions
+    {
+        public static TValue GetOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : default(TValue);
+        }
+
+        public static TValue GetOrEmpty<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key) where TValue : new()
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : new TValue();
+        }
+
+        public static TValue GetOrValue<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, TValue replacement)
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : replacement;
+        }
+
+        public static TValue GetOrValue<TKey, TValue>(
+            this IReadOnlyDictionary<TKey, TValue> dictionary, 
+            TKey key, 
+            Func<TKey, TValue> valueFunc)
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : valueFunc(key);
+        }
+    }
+}
---
+++ test/Infrastructure/Common.Tests/Extensions/DictionaryExtensionsTests.cs
@@ -0,0 +1,129 @@
+﻿namespace Skeleton.Common.Tests.Extensions
+{
+    using System.Collections.Generic;
+    using Common.Extensions;
+    using Xunit;
+
+    public class DictionaryExtensionsTests
+    {
+        [Fact]
+        public void GetOrDefaultShouldReturnExistedValue()
+        {
+            // Given
+            const int key = 1;
+            const int expectedValue = 1;
+            var dictionary = new Dictionary<int, int> {{key, expectedValue}};
+
+            // When
+            var actualValue = dictionary.GetOrDefault(key);
+
+            // Then
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void GetOrDefaultShouldReturnDefaultValueForTypeIfKeyNotExist()
+        {
+            // Given
+            const int key = 1;
+            var dictionary = new Dictionary<int, object>();
+
+            // When
+            var actualValue = dictionary.GetOrDefault(key);
+
+            // Then
+            object expectedValue = null;
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void GetOrEmptyShouldReturnExistedValue()
+        {
+            // Given
+            const int key = 1;
+            const int expectedValue = 1;
+            var dictionary = new Dictionary<int, int> {{key, expectedValue}};
+
+            // When
+            var actualValue = dictionary.GetOrEmpty(key);
+
+            // Then
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void GetOrEmptyShouldReturnDefaultConstructorCallResultIfKeyNotExist()
+        {
+            // Given
+            const int key = 1;
+            var dictionary = new Dictionary<int, List<int>>();
+
+            // When
+            var actualValue = dictionary.GetOrEmpty(key);
+
+            // Then
+            Assert.Empty(actualValue);
+        }
+
+        [Fact]
+        public void GetOrValueShouldReturnExistedValue()
+        {
+            // Given
+            const int key = 1;
+            const int expectedValue = 1;
+            var dictionary = new Dictionary<int, int> {{key, expectedValue}};
+
+            // When
+            var actualValue = dictionary.GetOrValue(key, 5);
+
+            // Then
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void GetOrValueShouldReturnPredefinedValueIfKeyNotExist()
+        {
+            // Given
+            const int key = 1;
+            const int expectedValue = 5;
+            var dictionary = new Dictionary<int, int>();
+
+            // When
+            var actualValue = dictionary.GetOrValue(key, expectedValue);
+
+            // Then
+            Assert.Equal(expectedValue, actualValue); 
+        }
+
+        [Fact]
+        public void GetOrValueWithFuncShouldReturnExistedValue()
+        {
+            // Given
+            const int key = 1;
+            const int expectedValue = 1;
+            var dictionary = new Dictionary<int, int> {{key, expectedValue}};
+
+            // When
+            var actualValue = dictionary.GetOrValue(key, x => 5);
+
+            // Then
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void GetOrValueWithFuncShouldReturnPredefinedValueIfKeyNotExist()
+        {
+            // Given
+            const int key = 1;
+            int ValueFunc(int x) => x + 1;
+            var dictionary = new Dictionary<int, int>();
+
+            // When
+            var actualValue = dictionary.GetOrValue(key, ValueFunc);
+
+            // Then
+            var expectedValue = ValueFunc(key);
+            Assert.Equal(expectedValue, actualValue);
+        }
+    }
+}
