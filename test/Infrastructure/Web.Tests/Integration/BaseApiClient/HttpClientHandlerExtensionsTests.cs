cc8466326677bf1d6e851ac595051e7a8b26df91
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2018-07-22 17:57:15 +0500

    Tests for HttpClientHandler extensions were added

---
+++ test/Infrastructure/Web.Tests/Integration/BaseApiClient/HttpClientHandlerExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Skeleton.Web.Tests.Integration.BaseApiClient
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using Web.Integration.BaseApiClient.Configuration;
+    using Xunit;
+
+    public class HttpClientHandlerExtensionsTests
+    {
+        [Fact]
+        public void WithAutomaticDecompressionShouldValidateParameters()
+        {
+            Assert.Throws<ArgumentNullException>(() => HttpClientHandlerExtensions.WithAutomaticDecompression(null));
+        }
+
+        [Fact]
+        public void ShouldChangeCompressionSettings()
+        {
+            // Given
+            var handler = new HttpClientHandler();
+
+            // When
+            handler.WithAutomaticDecompression();
+
+            // Then
+            Assert.Equal(DecompressionMethods.Deflate | DecompressionMethods.GZip, handler.AutomaticDecompression);
+        }
+    }
+}
