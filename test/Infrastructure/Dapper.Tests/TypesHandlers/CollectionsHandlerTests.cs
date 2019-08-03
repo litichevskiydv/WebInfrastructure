13a9587c6d3278c9735569e273f873805da5057d
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2019-08-03 19:50:38 +0500

    TheoryData was used for Dapper's collection handler tests

--- test/Infrastructure/Dapper.Tests/TypesHandlers/CollectionsHandlerTests.cs
+++ test/Infrastructure/Dapper.Tests/TypesHandlers/CollectionsHandlerTests.cs
@@ -1,7 +1,6 @@
 ﻿namespace Skeleton.Dapper.Tests.TypesHandlers
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using Common.Extensions;
     using Dapper.TypesHandlers;
@@ -11,6 +10,8 @@
 
     public class CollectionsHandlerTests : DbUsingTestBase
     {
+        #region TestCases
+
         public class TestEntity
         {
             public int Id { get; set; }
@@ -36,74 +37,82 @@
             public int Value { get; set; }
         }
 
+        public class CollectionHandlerUsageTestCase
+        {
+            public bool HandleEmptyCollectionAsNull { get; set; }
+
+            public TestEntity Value { get; set; }
+
+            public TestEntity Expected { get; set; }
+        }
+
+        #endregion
+
         [UsedImplicitly]
-        public static IEnumerable<object[]> CollectionsHandlerUsageTestsData;
+        public static TheoryData<CollectionHandlerUsageTestCase> CollectionHandlerUsageTestCases;
 
         static CollectionsHandlerTests()
         {
-            CollectionsHandlerUsageTestsData =
-                new[]
+            CollectionHandlerUsageTestCases =
+                new TheoryData<CollectionHandlerUsageTestCase>
                 {
-                    new object[]
+                    new CollectionHandlerUsageTestCase
                     {
-                        false,
-                        new TestEntity {Values = new[] {1, 2, 3}},
-                        new TestEntity {Values = new[] {1, 2, 3}}
+                        HandleEmptyCollectionAsNull = false,
+                        Value = new TestEntity {Values = new[] {1, 2, 3}},
+                        Expected = new TestEntity {Values = new[] {1, 2, 3}}
                     },
-                    new object[]
+                    new CollectionHandlerUsageTestCase
                     {
-                        false,
-                        new TestEntity {Values = new int[0]},
-                        new TestEntity {Values = new int[0]}
+                        HandleEmptyCollectionAsNull = false,
+                        Value = new TestEntity {Values = new int[0]},
+                        Expected = new TestEntity {Values = new int[0]}
                     },
-                    new object[]
+                    new CollectionHandlerUsageTestCase
                     {
-                        false,
-                        new TestEntity {Values = null},
-                        new TestEntity {Values = null}
+                        HandleEmptyCollectionAsNull = false,
+                        Value = new TestEntity {Values = null},
+                        Expected = new TestEntity {Values = null}
                     },
-                    new object[]
+                    new CollectionHandlerUsageTestCase
                     {
-                        true,
-                        new TestEntity {Values = new[] {1, 2, 3}},
-                        new TestEntity {Values = new[] {1, 2, 3}}
+                        HandleEmptyCollectionAsNull = true,
+                        Value = new TestEntity {Values = new[] {1, 2, 3}},
+                        Expected = new TestEntity {Values = new[] {1, 2, 3}}
                     },
-                    new object[]
+                    new CollectionHandlerUsageTestCase
                     {
-                        true,
-                        new TestEntity {Values = new int[0]},
-                        new TestEntity {Values = null}
+                        HandleEmptyCollectionAsNull = true,
+                        Value = new TestEntity {Values = new int[0]},
+                        Expected = new TestEntity {Values = null}
                     },
-                    new object[]
+                    new CollectionHandlerUsageTestCase
                     {
-                        true,
-                        new TestEntity {Values = null},
-                        new TestEntity {Values = null}
+                        HandleEmptyCollectionAsNull = true,
+                        Value = new TestEntity {Values = null},
+                        Expected = new TestEntity {Values = null}
                     }
                 };
         }
 
         [Theory]
-        [MemberData(nameof(CollectionsHandlerUsageTestsData))]
-        public void ShouldCollectionsHandler(
-            bool handleEmptyCollectionAsNull,
-            TestEntity source,
-            TestEntity expected)
+        [MemberData(nameof(CollectionHandlerUsageTestCases))]
+        public void ShouldCollectionsHandler(CollectionHandlerUsageTestCase testCase)
         {
             // Given
-            SqlMapper.AddTypeHandler(typeof(int[]), new CollectionsHandler<int>(handleEmptyCollectionAsNull));
+            SqlMapper.AddTypeHandler(typeof(int[]), new CollectionsHandler<int>(testCase.HandleEmptyCollectionAsNull));
 
             // When
             TestEntity actual;
             using (var connection = SqlConnectionsFactoryMethod())
             {
                 connection.Execute(@"create table #TestEntities (Id int identity(1, 1) not null, [Values] nvarchar(max) null)");
-                connection.Execute("insert into #TestEntities ([Values]) values(@Values)", source);
+                connection.Execute("insert into #TestEntities ([Values]) values(@Values)", testCase.Value);
                 actual = connection.Query<TestEntity>("select * from #TestEntities").Single();
             }
 
             // Then
-            Assert.Equal(expected, actual);
+            Assert.Equal(testCase.Expected, actual);
         }
 
         [Fact]
