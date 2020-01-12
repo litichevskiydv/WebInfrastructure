a9178ecf7a5fc6e34b195629847cba81d4ac14ac
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-01-12 20:50:09 +0300

    JwtBearer authentication scheme was configured explicitly

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -8,6 +8,7 @@
     using Domain.Dtos;
     using Installers;
     using JetBrains.Annotations;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
@@ -63,27 +64,31 @@
 
         protected override void ConfigureOptions(IServiceCollection services)
         {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokensSigningKey"]));
             services
                 .Configure<DefaultConfigurationValues>(Configuration.GetSection("DefaultConfigurationValues"))
                 .Configure<SqlConnectionsFactoryOptions>(Configuration.GetSection("ConnectionStrings"))
-                .AddJwtBearerAuthorizationTokens(
-                    b => b
-                        .ConfigureSigningKey(
-                            SecurityAlgorithms.HmacSha256,
-                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokensSigningKey"])))
-                        .ConfigureTokensIssuingOptions(
-                            i => i
-                                .WithGetEndpoint("/api/Account/Token")
-                                .WithLifetime(TimeSpan.FromHours(2))
-                                .WithTokenIssueEventHandler(
-                                    new TokenIssueEventHandler(LoggerFactory.CreateLogger<TokenIssueEventHandler>())))
-                        .ConfigureJwtBearerOptions(
-                            o => o
-                                .WithTokenValidationParameters(
-                                    v => v
-                                        .WithLifetimeValidation()
-                                        .WithoutAudienceValidation()
-                                        .WithoutIssuerValidation())));
+                .Configure<TokensIssuingOptions>(
+                    issuingOptions => issuingOptions
+                        .WithSigningKey(SecurityAlgorithms.HmacSha256, securityKey)
+                        .WithGetEndpoint("/api/Account/Token")
+                        .WithLifetime(TimeSpan.FromHours(2))
+                        .WithTokenIssueEventHandler(new TokenIssueEventHandler(LoggerFactory.CreateLogger<TokenIssueEventHandler>()))
+                )
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(
+                    jwtBearerOptions => jwtBearerOptions
+                        .WithTokenValidationParameters(
+                            parameters => parameters
+                                .WithIssuerKeyValidation(securityKey)
+                                .WithLifetimeValidation()
+                                .WithoutAudienceValidation()
+                                .WithoutIssuerValidation()
+                        )
+                );
+
+
+
         }
 
         protected override void RegisterDependencies(ContainerBuilder containerBuilder)
