fb9a9cf479534e256100114e9eb059a1c4dbfed7
litichevskiydv <litichevskiydv@gmail.com>
2021-07-04 22:52:33 +0300

    Useless code was removed

--- test/Infrastructure/Dapper.Tests/DbUsingTestBase.cs
+++ test/Infrastructure/Dapper.Tests/DbUsingTestBase.cs
@@ -9,22 +9,16 @@
     [UsedImplicitly]
     public abstract class DbUsingTestBase
     {
-        private readonly bool _isAppVeyorWindows;
-        private readonly bool _isAppVeyorLinux;
-        private readonly bool _isTravis;
+        private readonly bool _isAppVeyor;
         private readonly bool _isGithubActions;
 
         private string ConnectionString
         {
             get
             {
-                if (_isAppVeyorWindows)
+                if (_isAppVeyor)
                     return @"Data Source = (local)\SQL2017;Initial Catalog=tempdb;User Id=sa;Password=Password12!";
-                if (_isAppVeyorLinux)
-                    return "Data Source = localhost;Initial Catalog=tempdb;User Id=sa;Password=Password12!";
                 if (_isGithubActions)
-                    return "Data Source = localhost;Initial Catalog=tempdb;User Id=sa;Password=Password12!";
-                if (_isTravis)
                     return "Data Source = localhost;Initial Catalog=tempdb;User Id=sa;Password=Password12!";
 
                 return @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = tempdb; Integrated Security = True";
@@ -36,12 +30,7 @@
 
         protected DbUsingTestBase()
         {
-            var isAppVeyor = Environment.GetEnvironmentVariable("APPVEYOR")?.ToUpperInvariant() == "TRUE";
-            _isAppVeyorWindows = isAppVeyor && Environment.GetEnvironmentVariable("CI_WINDOWS")?.ToUpperInvariant() == "TRUE";
-            _isAppVeyorLinux = isAppVeyor && Environment.GetEnvironmentVariable("CI_LINUX")?.ToUpperInvariant() == "TRUE";
-
-            _isTravis = Environment.GetEnvironmentVariable("TRAVIS")?.ToUpperInvariant() == "TRUE";
-
+            _isAppVeyor = Environment.GetEnvironmentVariable("APPVEYOR")?.ToUpperInvariant() == "TRUE";
             _isGithubActions = Environment.GetEnvironmentVariable("GITHUB_ACTIONS")?.ToUpperInvariant() == "TRUE";
 
             ConnectionsFactory = new SqlConnectionsFactory(Options.Create(new SqlConnectionsFactoryOptions {SqlServer = ConnectionString}));
